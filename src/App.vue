f7accb261ea32f8256cabc3a0221160c280380a1
DragonEmperor9480 <amruteshnaregal1234@gmail.com>
2025-02-18 11:06:12 +0530

    Add: Responsive navbar with sorting options and cart icon

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,15 @@
 <script setup lang="ts">
-import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
+import { RouterView } from 'vue-router'
+import NavBar from './components/NavBar.vue'
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-    </div>
-  </header>
-
-  <RouterView />
+  <div class="min-h-screen bg-cream-white">
+    <NavBar />
+    <main class="container mx-auto px-4 py-8">
+      <RouterView />
+    </main>
+  </div>
 </template>
 
 <style scoped>
